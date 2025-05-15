8eb6032f216b8ee148e0a6750f02b14485e50212
mtito <matias.tito@gmail.com>
2025-05-15 15:27:32 -0300

    Entrega 2.

--- Agregador/src/main/java/ar/edu/utn/frba/dds/modelo/fuente/Fuente.java
+++ Agregador/src/main/java/ar/edu/utn/frba/dds/modelo/fuente/Fuente.java
@@ -1,7 +1,11 @@
 package ar.edu.utn.frba.dds.modelo.fuente;
 
 import static org.springframework.web.client.RestClient.create;
+import ar.edu.utn.frba.dds.web.dto.HechoDTO;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public class Fuente {
   private String baseUrl;
@@ -10,11 +14,12 @@ public class Fuente {
     this.baseUrl = baseUrl;
   }
 
-  public String hechos() {
-    ResponseEntity<String> result = create("http://" + baseUrl + "/hechos").
+  public List<HechoDTO> hechos() {
+    ResponseEntity<List<HechoDTO>> result = create("http://" + baseUrl + "/hechos").
         get()
         .retrieve()
-        .toEntity(String.class);
+        .toEntity(new ParameterizedTypeReference<List<HechoDTO>>() {
+        });
     return result.getBody();
   }
 }
