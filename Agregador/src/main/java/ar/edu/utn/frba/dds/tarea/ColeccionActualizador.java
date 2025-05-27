470b7ee148e1a84934b4efb83afbc68bcda19e23
matiastito <matias.tito@gmail.com>
2025-05-27 01:22:04 -0300

    2da Entrega

--- Agregador/src/main/java/ar/edu/utn/frba/dds/tarea/ColeccionActualizador.java
+++ Agregador/src/main/java/ar/edu/utn/frba/dds/tarea/ColeccionActualizador.java
@@ -1,5 +1,8 @@
 package ar.edu.utn.frba.dds.tarea;
 
+import static ar.edu.utn.frba.dds.modelo.fuente.TipoFuente.PROXY;
+
+import ar.edu.utn.frba.dds.modelo.coleccion.Coleccion;
 import ar.edu.utn.frba.dds.repositorio.ColeccionRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -7,12 +10,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ColeccionActualizador {
-
   @Autowired
   private ColeccionRepositorio coleccionRepositorio;
 
   @Scheduled(fixedRate = 5000)
   public void actulizarColecciones() {
-    coleccionRepositorio.colleciones().forEach(coleccion -> coleccion.colectarHechos());
+    coleccionRepositorio.colleciones().stream()
+        .filter(c -> !PROXY.equals(c.getFuente().getTipoFuente()))
+        .forEach(Coleccion::colectarHechos);
   }
 }
