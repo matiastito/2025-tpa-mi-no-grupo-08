7e8650b70bfc1506412559b889f140e14e560ea5
matiastito <matias.tito@gmail.com>
2025-11-23 12:55:34 -0300

    Probando Deploy.

--- FuenteProxy/src/main/java/ar/edu/utn/frba/dds/modelo/fuente/cache/CacheHechos.java
+++ FuenteProxy/src/main/java/ar/edu/utn/frba/dds/modelo/fuente/cache/CacheHechos.java
@@ -20,7 +20,7 @@ public class CacheHechos {
   public Collection<HechoDTO> getHechos() {
     return hechos;
   }
-
+  
   public void actualizar(Collection<HechoDTO> hechos) {
     this.hechos.clear();
     this.hechos.addAll(hechos);
